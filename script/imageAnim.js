1842b10348afb53c9b6d188176287fb1a2bb070a
Luisa Valero <luisa.valero19@gmail.com>
2019-02-04 16:37:18 -0500

    Adding more js, css and html

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -8,18 +8,56 @@
        puzzleBoards = document.querySelector(".puzzle-board"),
        puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+   let dropZones = document.querySelectorAll('.drop-zone');
+
    //functions go in the middle
    function createPuzzlePieces(pictureIndex){
    	//generate puzzle pieces for the left hand side
    	// debugger;
    	pieces.forEach((piece, index) => {
-   		let newPuzzlePiece = `<img id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
+   		let newPuzzlePiece = `<img draggable id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
    		piecesBoards.innerHTML+= newPuzzlePiece;
    	});
 
-   	puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`;
+   	puzzleBoards.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`;
+
+      initDrag();
 
    }    
+
+
+   // drag and drop functionality goes here
+
+   function initDrag() {
+      piecesBoards.querySelectorAll('img').forEach(img => {
+         img.addEventListener("dragstart", function(e) {
+            // e.preventDefault();
+            console.log('draggin...');
+
+            e.dataTransfer.setData("text/plain", this.id)
+         });
+      });
+
+   }
+
+   // handle dragover and drop 
+   dropZones.forEach(zone => {
+      zone.addEventListener("dragover", function(e) {
+         e.preventDefault();
+         console.log("you dragged over me!");
+      });
+
+      zone.addEventListener("drop", function(e) {
+         e.preventDefault();
+         console.log("you dropped sumpin on me!");
+
+         let piece = e.dataTransfer.getData("text/plain");
+         e.target.appendChild(document.querySelector(`#${piece}`));
+
+      });
+
+   });
+
 
    function resetPuzzlePieces(){
    	//empty the thumbnail container
