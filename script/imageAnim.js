b372555ec1f52d4580f38acb4db4659f00bae549
Luisa Valero <luisa.valero19@gmail.com>
2019-03-03 13:07:06 -0500

    Adding firstbug

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -15,13 +15,13 @@
    	//generate puzzle pieces for the left hand side
    	// debugger;
    	pieces.forEach((piece, index) => {
-   		let newPuzzlePiece = `<img draggable id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
-   		piecesBoards.innerHTML+= newPuzzlePiece;
+   	let newPuzzlePiece = `<img draggable id="pieces${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
+   	piecesBoards.innerHTML+= newPuzzlePiece;
    	});
 
    	puzzleBoards.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`;
 
-      initDrag();
+    initDrag();
 
    }
 
@@ -40,14 +40,14 @@
 
    }
 
-   // handle dragover and drop
-   dropZones.forEach(zone => {
-      zone.addEventListener("dragover", function(e) {
+         // handle dragover and drop
+         dropZones.forEach(zone => {
+         zone.addEventListener("dragover", function(e) {
          e.preventDefault();
          console.log("you dragged over me!");
       });
 
-      zone.addEventListener("drop", function(e) {
+         zone.addEventListener("drop", function(e) {
          e.preventDefault();
          console.log("you dropped sumpin on me!");
 
@@ -60,19 +60,19 @@
 
    //Reset each of the drop zones
    function resetPuzzlePieces(){
-   	//empty the thumbnail container
+    //empty the thumbnail container
+    debugger;
    	piecesBoards.innerHTML = "";
    	createPuzzlePieces(this.dataset.puzzleref);
-    debugger;
-    piecesBoards.innerHTML = "";
-    createPuzzlePieces(this.dataset.puzzleref);
     // foreach has specify a callback function in the array
-    dropZones.forEach(zone => {
-    console.log(zone);
     // empty the drop zones
-    zone.innerHTML = "";
+    dropZones.forEach(zone => {
+    // removeChild
+    while (zone.firstChild) zone.removeChild(zone.firstChild);
     });
-   }
+    }
+    // for (var i = 0, zone = dropZones.length; i < zone; i++) {
+    // someFn(dropZones[i]);
 
    //event handling down here
    puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
